b51d14475c466f8698ae705cf8619470a6b127d7
Dominik Bialkowski <41293638+dbialkowskipgs@users.noreply.github.com>
2024-03-07 13:43:00 +0000

    Copilot third point thre commit

--- comments.js
+++ comments.js
@@ -1,2 +1,14 @@
-// create web serwer
-const express = require('express');
+// create web serwer for comment
+
+var express = require('express');
+var router = express.Router();
+
+var Comment = require('../models/comment');
+
+router.get('/comments', function(req, res, next) {
+  Comment.find(function(err, comments) {
+    if (err) {
+      return next(err);
+    }
+  });
+});
